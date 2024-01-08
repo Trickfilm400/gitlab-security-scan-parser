92150e3aa3118ecbe3f343503cfd2aa1078043ae
Nico Wagner <info@ni-wa.de>
2024-01-08 06:58:07 +0100

    fix: add debug information to console

--- src/index.ts
+++ src/index.ts
@@ -24,11 +24,31 @@ try {
   console.log(e);
 }
 console.log(fs.readdirSync(path.join("/builds")));
-dir.files("/", function (_err, content) {
-  //if (err) throw err;
-  console.log("content:", content);
-  console.log(JSON.stringify(content));
-});
+function searchFile(dir: string, fileName: string) {
+  // read the contents of the directory
+  const files = fs.readdirSync(dir);
+
+  // search through the files
+  for (const file of files) {
+    // build the full path of the file
+    const filePath = path.join(dir, file);
+
+    // get the file stats
+    const fileStat = fs.statSync(filePath);
+
+    // if the file is a directory, recursively search the directory
+    if (fileStat.isDirectory()) {
+      searchFile(filePath, fileName);
+    } else if (file.endsWith(fileName)) {
+      // if the file is a match, print it
+      console.log(filePath);
+    }
+  }
+}
+
+// start the search in the current directory
+searchFile("/builds", Filenames.CONTAINER);
+searchFile("/", Filenames.CONTAINER);
 dir.files("/builds", function (err, content) {
   if (err) throw err;
   console.log("content:", content);
